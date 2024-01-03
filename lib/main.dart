90ecb3c537a7675b144dc671a5e96ed89e76a77b
risheetajain <risheetajain309@gmail.com>
2024-01-03 17:26:07 +0530

    dotenv added

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,14 @@
 import 'dart:convert';
 
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_stripe/flutter_stripe.dart';
 import 'package:http/http.dart' as http;
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  Stripe.publishableKey =
-      'pk_test_51JFH9zSGTdxZA1VVPlzCM1b4ztYvbz452v792r5iofLUkOdc15YdKHAv6VLSkt7qT5l643GIanpkbi8YCAQo47fm004YSyva3s';
+  dotenv.load(fileName: "lib/.env");
+  Stripe.publishableKey = dotenv.env["PUBLISH_KEY"] ?? "";
 
   runApp(const MyApp());
 }
@@ -17,21 +17,23 @@ class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      home: Payment(),
+      theme: ThemeData(primaryColor: Colors.green),
+      title: 'Flutter Stripe Template',
+      home: const StripePaymentScreen(),
     );
   }
 }
 
-class Payment extends StatefulWidget {
-  const Payment({Key? key}) : super(key: key);
+class StripePaymentScreen extends StatefulWidget {
+  const StripePaymentScreen({super.key});
+
   @override
-  _HomeScreenState createState() => _HomeScreenState();
+  State<StripePaymentScreen> createState() => _StripePaymentScreenState();
 }
 
-class _HomeScreenState extends State<Payment> {
+class _StripePaymentScreenState extends State<StripePaymentScreen> {
   Map<String, dynamic>? paymentIntent;
 
   @override
@@ -41,7 +43,7 @@ class _HomeScreenState extends State<Payment> {
         title: const Text('Stripe Payment'),
       ),
       body: Center(
-        child: TextButton(
+        child: ElevatedButton(
           child: const Text('Make Payment'),
           onPressed: () async {
             await makePayment();
@@ -53,65 +55,53 @@ class _HomeScreenState extends State<Payment> {
 
   Future<void> makePayment() async {
     try {
+      //Create payment intent data
       paymentIntent = await createPaymentIntent('10', 'INR');
+      //initialise the payment sheet setup
       await Stripe.instance.initPaymentSheet(
         paymentSheetParameters: SetupPaymentSheetParameters(
-
+          //Client secret key from payment data
           paymentIntentClientSecret: paymentIntent!['client_secret'],
           googlePay: const PaymentSheetGooglePay(
-              testEnv: true, currencyCode: "USD", merchantCountryCode: "IN"),
-          merchantDisplayName: 'anjali',
-          customerEphemeralKeySecret: paymentIntent!['ephemeralKey'],
-         returnURL: 'flutterstripe://redirect',
+              //Currency and country code is accourding to India
+              testEnv: true,
+              currencyCode: "INR",
+              merchantCountryCode: "IN"),
+          //Merchant Name
+          merchantDisplayName: 'Flutterwings',
+//return URl if you want to add
+          // returnURL: 'flutterstripe://redirect',
         ),
       );
+      //Display payment sheet
       displayPaymentSheet();
     } catch (e) {
       print("exception $e");
-      if (kDebugMode) {
-        if (e is StripeConfigException) {
-          print("Stripe exception ${e.message}");
-        } else {
-          print("exception $e");
-        }
+
+      if (e is StripeConfigException) {
+        print("Stripe exception ${e.message}");
+      } else {
+        print("exception $e");
       }
     }
   }
 
   displayPaymentSheet() async {
     try {
-      print("Display payment sheet");
+      // "Display payment sheet";
       await Stripe.instance.presentPaymentSheet();
-      print("Displayed successfully");
-      // showDialog(
-      //   context: context,
-      //   builder: (_) => const AlertDialog(
-      //     content: Column(
-      //       mainAxisSize: MainAxisSize.min,
-      //       children: [
-      //         Row(
-      //           children: [
-      //             Icon(Icons.check_circle, color: Colors.green),
-      //             Text("Payment Successful"),
-      //           ],
-      //         ),
-      //       ],
-      //     ),
-      //   ),
-      // );
-
+      //Show when payment is done
+//Displaying snackbar for it
       ScaffoldMessenger.of(context).showSnackBar(
         const SnackBar(content: Text("Paid successfully")),
       );
       paymentIntent = null;
     } on StripeException catch (e) {
+      //If any error comes during payment so payment will be cancelled
       print('Error: $e');
 
-      showDialog(
-        context: context,
-        builder: (_) => const AlertDialog(
-          content: Text("Cancelled"),
-        ),
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(content: Text(" Payment Cancelled")),
       );
     } catch (e) {
       print("Error in displaying");
@@ -122,12 +112,11 @@ class _HomeScreenState extends State<Payment> {
   createPaymentIntent(String amount, String currency) async {
     try {
       Map<String, dynamic> body = {
-        'amount': calculateAmount(amount),
+        'amount': ((int.parse(amount)) * 100).toString(),
         'currency': currency,
         'payment_method_types[]': 'card',
       };
-      var secretKey =
-          "sk_test_51JFH9zSGTdxZA1VV3MtP5wDpatKDerB7R6gdV2Vk4h5Qs6AMpng9Yke15xZzDQveyqt8NFPQ36pAiEgAfn8x5nNh00i7PPFMej";
+      var secretKey = dotenv.env["SECRET_KEY"];
       var response = await http.post(
         Uri.parse('https://api.stripe.com/v1/payment_intents'),
         headers: {
@@ -142,10 +131,4 @@ class _HomeScreenState extends State<Payment> {
       print('Error charging user: ${err.toString()}');
     }
   }
-
-  calculateAmount(String amount) {
-    final calculatedAmount = (int.parse(amount)) * 100;
-    return calculatedAmount.toString();
-  }
-// Implement other methods here
 }
